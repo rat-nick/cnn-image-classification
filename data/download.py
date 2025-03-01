d2f5bde73317114dda8c577e45d9bdc33cf24726
rat-nick <ratinac.nikola@gmail.com>
2025-03-01 19:28:50 +0100

    add csv-file param to download script

--- data/download.py
+++ data/download.py
@@ -1,31 +1,28 @@
-import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
-
 import aiohttp
 import asyncio
 import aiofiles
 import logging
 import pandas as pd
+import click
 from tqdm.asyncio import tqdm
 
-
+from config import IMAGES_PATH, LOG_PATH, DATA_PATH
+from utils import drop_missing_poster
 
 # Set up logging
-LOG_DIR = './logs'
-os.makedirs(LOG_DIR, exist_ok=True)
-logging.basicConfig(level=logging.ERROR, format='%(asctime)s %(levelname)s:%(message)s', filename=os.path.join(LOG_DIR, 'download.log'), filemode='a')
+os.makedirs(LOG_PATH, exist_ok=True)
+logging.basicConfig(level=logging.ERROR, format='%(asctime)s %(levelname)s:%(message)s', filename=os.path.join(LOG_PATH, 'download.log'), filemode='a')
 
 # Directory for storing images
-IMAGE_DIR = './images'
-os.makedirs(IMAGE_DIR, exist_ok=True)
+os.makedirs(IMAGES_PATH, exist_ok=True)
 
 # Limit the number of concurrent downloads
 CONCURRENT_DOWNLOADS = 100
 
 async def download_poster(session, poster_url, movie_id, semaphore):
     """ Asynchronously download a single poster image. """
-    poster_path = os.path.join(IMAGE_DIR, f'{movie_id}.jpg')
+    poster_path = os.path.join(IMAGES_PATH, f'{movie_id}.jpg')
 
     async with semaphore:
         try:
@@ -55,14 +52,14 @@ async def download_posters(df):
             results.append(await f)
 
     return results
-
-def main():
-    df = pd.read_csv('anime.csv')
-    from utils import drop_missing_poster
+    
+@click.command()
+@click.argument('csv-file', default='anime.csv')
+def main(csv_file):
+    df = pd.read_csv(DATA_PATH / csv_file)
     df = drop_missing_poster(df)
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
     asyncio.run(download_posters(df))
-    
 
 if __name__ == "__main__":
     main()
